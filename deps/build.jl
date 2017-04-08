aa19544ce947899a2f756463cdb3cd049e6cd265
Benoît Legat <benoit.legat@gmail.com>
2017-04-08 16:39:05 +0200

    Try Windows build

--- deps/build.jl
+++ deps/build.jl
@@ -9,8 +9,9 @@ libdir_opt = ""
 
 @BinDeps.setup
 
-blas = library_dependency("libblas", alias=["libblas.dll"])
-lapack = library_dependency("liblapack", alias=["liblapack.dll"])
+#blas = library_dependency("libblas", alias=["libblas.dll"])
+#lapack = library_dependency("liblapack", alias=["liblapack.dll"])
+depends = [] # [blas, lapack]
 
 official_download="http://downloads.sourceforge.net/project/sdpa/sdpa/sdpa_7.3.8.tar.gz?r=&ts=1479039688&use_mirror=vorboss"
 
@@ -21,10 +22,11 @@ sdpa_dir = joinpath(dirname(@__FILE__), "src", sdpaname)
 mumps_include_dir = joinpath(sdpa_dir, "mumps", "build", "include")
 cxx_wrap_dir = joinpath(dirname(@__FILE__), "..", "..", "CxxWrap", "deps", "usr", "lib", "cmake")
 
-sdpawrap = library_dependency("sdpawrap", aliases=["libsdpawrap"], depends=[blas, lapack])
+sdpa = library_dependency("sdpa", aliases=["sdpa-7.3.9"], depends=depends)
+sdpawrap = library_dependency("sdpawrap", aliases=["libsdpawrap"], depends=[depends; sdpa])
 
 provides(Sources,
-        Dict(URI(official_download) => sdpawrap), unpacked_dir=sdpaname)
+        Dict(URI(official_download) => sdpa), unpacked_dir=sdpaname)
 
 #includedirs = AbstractString[src_dir]
 #targetdirs = AbstractString["libsdpa.a"] #$(Libdl.dlext)"]
@@ -47,36 +49,31 @@ sdpawrap_builddir = joinpath(BinDeps.depsdir(sdpawrap), "builds", "sdpa_wrap")
 
 makeopts = ["--", "-j", "$(Sys.CPU_CORES+2)"]
 
-# Set generator if on windows
-genopt = "Unix Makefiles"
-@static if is_windows()
-  makeopts = "--"
-  if Sys.WORD_SIZE == 64
-    genopt = "Visual Studio 14 2015 Win64"
-    cmake_prefix = joinpath(QT_ROOT, "msvc2015_64")
-  else
-    genopt = "Visual Studio 14 2015"
-    cmake_prefix = joinpath(QT_ROOT, "msvc2015")
-  end
+# It needs to be a function so that it is called only when blas and lapack dependencies have been resolved
+function blas_lib()
+    if false
+        "-L$(dirname(first(BinDeps._find_library(blas))[2])) -lblas"
+    else
+        "-L$(Libdl.dlpath(LinAlg.BLAS.libblas))"
+    end
 end
 
-@show BinDeps._find_library(blas)
-@show dirname(first(BinDeps._find_library(blas))[2])
-@show BinDeps._find_library(lapack)
-@show dirname(first(BinDeps._find_library(lapack))[2])
-sdpa_steps = @build_steps begin
-       `cmake -G "$genopt" -DCMAKE_INSTALL_PREFIX="$prefix" -DCMAKE_BUILD_TYPE="Release" -DCxxWrap_DIR="$cxx_wrap_dir" -DSDPA_DIR="$sdpa_dir" -DMUMPS_INCLUDE_DIR="$mumps_include_dir" -DSDPA_LIBRARY="$sdpa_library" -DMUMPS_LIB_DIR="$mumps_lib_dir" -DMUMPS_LIBSEQ_DIR="$mumps_libseq_dir" -DBLAS_DIR="$(dirname(first(BinDeps._find_library(blas))[2]))" -DLAPACK_DIR="$(dirname(first(BinDeps._find_library(lapack))[2]))" $sdpawrap_srcdir`
-       `cmake --build . --config Release --target install $makeopts`
+function lapack_lib()
+    if false
+        "-L$(dirname(first(BinDeps._find_library(blas))[2])) -llapack"
+    else
+        "-L$(Libdl.dlpath(LinAlg.LAPACK.liblapack))"
+    end
 end
 
 provides(BuildProcess,
 (@build_steps begin
-    GetSources(sdpawrap)
+    GetSources(sdpa)
     CreateDirectory(sdpaprefixdir)
     CreateDirectory(sdpalibdir)
     @build_steps begin
         ChangeDirectory(sdpasrcdir)
-        FileRule(joinpath(sdpa_library),@build_steps begin
+        FileRule(joinpath(sdpa_library), @build_steps begin
             # See https://sourceforge.net/p/sdpa/discussion/1393613/thread/1a6d8897/
             pipeline(`sed "s/OPTF = \"/OPTF = \" '-I\$\$(topdir)\/libseq'/" mumps/Makefile`, stdout="mumpsMakefile")
             `mv mumpsMakefile mumps/Makefile`
@@ -87,15 +84,39 @@ provides(BuildProcess,
             pipeline(`sed 's/AC_FC_LIBRARY/LT_INIT\nAC_FC_LIBRARY/' configure.in`, stdout="configure.ac")
             `rm configure.in`
             `autoreconf -i`
-            `./configure CFLAGS=-funroll-all-loops CXXFLAGS=-funroll-all-loops FFLAGS=-funroll-all-loops --with-blas="-L$(dirname(first(BinDeps._find_library(blas))[2])) -lblas" --with-lapack="-L$(dirname(first(BinDeps._find_library(lapack))[2])) -llapack"`
+            `./configure CFLAGS=-funroll-all-loops CXXFLAGS=-funroll-all-loops FFLAGS=-funroll-all-loops --with-blas="$(blas_lib())" --with-lapack="$(lapack_lib())"`
             `make`
             `cp .libs/$target .libs/$target.0 $sdpalibdir` # It seems that sdpawrap links itself with $target.0
         end)
     end
+end), sdpa, os=:Windows)
+
+# The zip on SDPA does not contain DLLs, only an EXE :(
+# provides(Binaries,
+#     URI("https://sourceforge.net/projects/sdpa/files/sdpa/windows/sdpa-7.3.9-windows.zip"),
+#     [sdpa], unpacked_dir="usr", os = :Windows)
+
+# Set generator if on windows
+genopt = "Unix Makefiles"
+@static if is_windows()
+  makeopts = "--"
+  if Sys.WORD_SIZE == 64
+    genopt = "Visual Studio 14 2015 Win64"
+  else
+    genopt = "Visual Studio 14 2015"
+  end
+end
+
+provides(BuildProcess,
+(@build_steps begin
     CreateDirectory(sdpawrap_builddir)
     @build_steps begin
         ChangeDirectory(sdpawrap_builddir)
-        FileRule(joinpath(prefix, "lib$libdir_opt", "$(lib_prefix)sdpawrap.$(Libdl.dlext)"), sdpa_steps)
+        FileRule(joinpath(prefix, "lib$libdir_opt", "$(lib_prefix)sdpawrap.$(Libdl.dlext)"),
+            @build_steps begin
+               `cmake -G "$genopt" -DCMAKE_INSTALL_PREFIX="$prefix" -DCMAKE_BUILD_TYPE="Release" -DCxxWrap_DIR="$cxx_wrap_dir" -DSDPA_DIR="$sdpa_dir" -DMUMPS_INCLUDE_DIR="$mumps_include_dir" -DSDPA_LIBRARY="$sdpa_library" -DMUMPS_LIB_DIR="$mumps_lib_dir" -DMUMPS_LIBSEQ_DIR="$mumps_libseq_dir" -DBLAS_LIB="$(blas_lib())" -DLAPACK_LIB="$(lapack_lib())" $sdpawrap_srcdir`
+               `cmake --build . --config Release --target install $makeopts`
+            end)
     end
     end), sdpawrap)
 
