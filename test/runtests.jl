847021859fa9ef7c0aae08bf53bbe059e51e49aa
Benoît Legat <benoit.legat@gmail.com>
2017-07-26 00:42:55 +0200

    Update from SemidefiniteModels to SemidefiniteOptInterface

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,12 @@ using Base.Test
 
 include("example1.jl")
 include("example2.jl")
-include("mathprogbase.jl")
+
+@testset "Linear tests" begin
+    include(joinpath(Pkg.dir("MathOptInterface"), "test", "contlinear.jl"))
+    contlineartest(SDPA.SDPASolver(), 1e-5)
+end
+@testset "Conic tests" begin
+    include(joinpath(Pkg.dir("MathOptInterface"), "test", "contconic.jl"))
+    contconictest(SDPA.SDPASolver(), 1e-5)
+end
