f2f464a3df8d8d018c16836a26e1b626738e8fbf
Juan Pablo Vielma <jvielma@mit.edu>
2019-01-22 19:30:56 -0500

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -20,23 +20,42 @@ download_info = Dict(
     Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7, :cxx11)) => ("$bin_prefix/SDPABuilder.v7.3.8.x86_64-linux-gnu-gcc7-cxx11.tar.gz", "52afa44d1b85b2bab129e0c925c50cb1b7c734a3fd0a787c0b229647750821e9"),
     Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8, :cxx11)) => ("$bin_prefix/SDPABuilder.v7.3.8.x86_64-linux-gnu-gcc8-cxx11.tar.gz", "90bf1b48fda853fdb5cbc51c3566bced5133c85c3cee3d900948345a1072a244"),
 )
-
-# Install unsatisfied or updated dependencies:
-unsatisfied = any(!satisfied(p; verbose=verbose) for p in products)
-dl_info = choose_download(download_info, platform_key_abi())
-if dl_info === nothing && unsatisfied
-    # If we don't have a compatible .tar.gz to download, complain.
-    # Alternatively, you could attempt to install from a separate provider,
-    # build from source or something even more ambitious here.
-    error("Your platform (\"$(Sys.MACHINE)\", parsed as \"$(triplet(platform_key_abi()))\") is not supported by this package!")
+                    
+custom_library = false
+if haskey(ENV,"JULIA_SDPA_LIBRARY_PATH")
+    custom_products = [LibraryProduct(ENV["JULIA_SDPA_LIBRARY_PATH"],product.libnames,product.variable_name) for product in products]
+    if all(satisfied(p; verbose=verbose) for p in custom_products)
+        products = custom_products
+        custom_library = true
+    else
+        error("Could not install custom libraries from $(ENV["JULIA_SDPA_LIBRARY_PATH"]).\nTo fall back to BinaryProvider call delete!(ENV,\"JULIA_SDPA_LIBRARY_PATH\") and run build again.")
+    end
 end
 
-# If we have a download, and we are unsatisfied (or the version we're
-# trying to install is not itself installed) then load it up!
-if unsatisfied || !isinstalled(dl_info...; prefix=prefix)
-    # Download and install binaries
-    install(dl_info...; prefix=prefix, force=true, verbose=verbose)
-end
+if !custom_library
+    # Install unsatisfied or updated dependencies:
+    unsatisfied = any(!satisfied(p; verbose=verbose) for p in products)
 
+    dl_info = choose_download(download_info, platform_key_abi())
+    if dl_info === nothing && unsatisfied
+        # If we don't have a compatible .tar.gz to download, complain.
+        # Alternatively, you could attempt to install from a separate provider,
+        # build from source or something even more ambitious here.
+        error("Your platform (\"$(Sys.MACHINE)\", parsed as \"$(triplet(platform_key_abi()))\") is not supported by this package!")
+    end
+
+    # If we have a download, and we are unsatisfied (or the version we're
+    # trying to install is not itself installed) then load it up!
+    if unsatisfied || !isinstalled(dl_info...; prefix=prefix)
+        # Download and install binaries
+        # no dynamic dependencies until Pkg3 support for binaries
+        # for dependency in reverse(dependencies)          # We do not check for already installed dependencies
+        #    download(dependency,basename(dependency))
+        #    evalfile(basename(dependency))
+        # end
+        install(dl_info...; prefix=prefix, force=true, verbose=verbose)
+    end
+ end
+                    
 # Write out a deps.jl file that will contain mappings for our products
 write_deps_file(joinpath(@__DIR__, "deps.jl"), products, verbose=verbose)
