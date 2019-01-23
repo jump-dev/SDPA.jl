f4a75308cb11e3bf5bcba1c895a77ec9019fcb56
Juan Pablo Vielma <jvielma@mit.edu>
2019-01-23 11:22:02 -0500

    Update SDPA.jl

--- src/SDPA.jl
+++ src/SDPA.jl
@@ -2,7 +2,7 @@ module SDPA
 
 using CxxWrap
 
-# This 'using' is required to suppress a warning about Clp not having Libdl in its
+# This 'using' is required to suppress a warning about SDPA not having Libdl in its
 # dependencies (Libdl is used by BinaryProvider), e.g.: bicycle1885/CodecZlib.jl#26.
 using Libdl
 
