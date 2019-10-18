4c33ed1db6a91d17ef7220351ffe5cd6ed712051
Benoît Legat <benoit.legat@gmail.com>
2019-10-19 00:10:30 +0200

    Remove support for SingleVariable objecive

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -93,8 +93,7 @@ end
 function MOI.supports(
     optimizer::Optimizer,
     ::Union{MOI.ObjectiveSense,
-            MOI.ObjectiveFunction{<:Union{MOI.SingleVariable,
-                                          MOI.ScalarAffineFunction{Cdouble}}}})
+            MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Cdouble}}})
     return true
 end
 
@@ -123,7 +122,7 @@ function MOIU.allocate(optimizer::Optimizer, ::MOI.ObjectiveSense, sense::MOI.Op
     # To be sure that it is done before load(optimizer, ::ObjectiveFunction, ...), we do it in allocate
     optimizer.objsign = sense == MOI.MIN_SENSE ? -1 : 1
 end
-function MOIU.allocate(::Optimizer, ::MOI.ObjectiveFunction, ::Union{MOI.SingleVariable, MOI.ScalarAffineFunction}) end
+function MOIU.allocate(::Optimizer, ::MOI.ObjectiveFunction, ::MOI.ScalarAffineFunction) end
 
 function MOIU.load(::Optimizer, ::MOI.ObjectiveSense, ::MOI.OptimizationSense) end
 # Loads objective coefficient α * vi
@@ -144,9 +143,6 @@ function MOIU.load(optimizer::Optimizer, ::MOI.ObjectiveFunction, f::MOI.ScalarA
             load_objective_term!(optimizer, t.coefficient, t.variable_index)
         end
     end
-end
-function MOIU.load(optimizer::Optimizer, ::MOI.ObjectiveFunction, f::MOI.SingleVariable)
-    load_objective_term!(optimizer, one(Cdouble), f.variable)
 end
 
 function new_block(optimizer::Optimizer, set::MOI.Nonnegatives)
